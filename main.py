6c71ad98944d514afd59ec96473147598da66ecf
ayesha786012 <59975636+ayesha786012@users.noreply.github.com>
2021-01-20 14:56:40 +0500

    Update main.py

--- main.py
+++ main.py
@@ -100,14 +100,15 @@ def patients():
             patientsfathername = request.form['patients_father_name']
             patientsaddressname = request.form['patients_Address_name']
             patientsEMAILaddressname = request.form['patients_email_Address_name']
-            patientsphone_number=request.form['patients_Phone_Number']
+            patientsphone_number = request.form['patients_Phone_Number']
             # password = request.form['password'].encode("ascii")
             # encoded = base64.b64encode(password)
             # decoded = encoded.decode('ascii', 'strict')
             cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
             cursor.execute(
                 "INSERT INTO logininfo.patients(patients_first_name,patients_last_name,Patients_father_name,Patient_address,Patients_email_address,patients_phone_number) VALUES(%s,%s,%s,%s,%s,%s)",
-                (patientsfname, patientslname, patientsfathername, patientsaddressname, patientsEMAILaddressname,patientsphone_number))
+                (patientsfname, patientslname, patientsfathername, patientsaddressname, patientsEMAILaddressname,
+                 patientsphone_number))
             db.connection.commit()
             cursor.close()
             cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
@@ -119,16 +120,14 @@ def patients():
                 print(user_details)
                 # db.connection.commit()
             return render_template("Patients.html", user_details=user_details)
-            #return render_template("pateints_record_register.html")
+            # return render_template("pateints_record_register.html")
             # return redirect(url_for(''))
-        #return redirect(url_for('get'))
+        # return redirect(url_for('get'))
 
     return render_template("Patients.html")
 
 
-#@app.route('/new/RECORD_patients')
-
-
+# @app.route('/new/RECORD_patients')
 
 
 @app.route('/new/action_page', methods=['GET', 'POST'])
@@ -139,59 +138,61 @@ def livesearch():
     cursor.execute(query)
     result = cursor.fetchall()
     return jsonify(result)
-#def lom():
-   # if request.method == 'POST':
-     #   if "search" in request.form:
-      #      search = request.form["search"]
-         #   print(search)
-          #  cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
-           # patients_RECORDed = cursor.execute("SELECT * FROM patients WHERE patients_first_name LIKE %s",
-                                              # ([search],))
-            # patients_RECORD = cursor.execute(")
-           # """Api to restive all the patient from the database"""
-          #  print(patients_RECORDed)
-          #  if patients_RECORDed > 0:
-           #     user_details = cursor.fetchall()
-            #    print(user_details)
-                # db.connection.commit()
-          #  return render_template("Patients.html", user_details=user_details)
 
-    #return render_template("Patients.html")
 
-#the method which is used to delete
+# def lom():
+# if request.method == 'POST':
+#   if "search" in request.form:
+#      search = request.form["search"]
+#   print(search)
+#  cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+# patients_RECORDed = cursor.execute("SELECT * FROM patients WHERE patients_first_name LIKE %s",
+# ([search],))
+# patients_RECORD = cursor.execute(")
+# """Api to restive all the patient from the database"""
+#  print(patients_RECORDed)
+#  if patients_RECORDed > 0:
+#     user_details = cursor.fetchall()
+#    print(user_details)
+# db.connection.commit()
+#  return render_template("Patients.html", user_details=user_details)
 
-#@app.route("/ajax_add",methods=["POST","GET"])
-#def ajax_add():
-    #if request.method == 'POST':
-        #cur = db.connection.cursor(MySQLdb.cursors.DictCursor)
-        #patients_first_name = request.form['patients_first_name']
-        #patients_last_name = request.form['patients_last_name']
-        ###Patient_address = request.form['Patient_address']
-        #Patients_email_address = request.form['Patients_email_address']
-        #patients_phone_number = request.form['patients_phone_number']
-        #print(txtname)
-        #if patients_first_name == '':
-            #msg = 'Please Input name'
-        #elif patients_last_name == '':
-           #msg = 'Please Input Department'
-        #elif Patients_father_name == '':
-           #msg = 'Please Input Phone'
-        #elif Patient_address == '':
-           #msg = 'Please Input Department'
-        #elif Patients_email_address == '':
-            #msg = 'Please Input Phone'
-        #elif patients_phone_number == '':
-            #msg = 'Please Input Phone'
-        #else:
-            #cur.execute(
-                #"INSERT INTO logininfo.patients(patients_first_name,patients_last_name,Patients_father_name,Patient_address,Patients_email_address,patients_phone_number) VALUES(%s,%s,%s,%s,%s,%s)",
-                #(patients_first_name, patients_last_name, Patients_father_name, Patient_address,Patients_email_address,patients_phone_number))
-            #db.connection.commit()
-            #cur.close()
-            #msg = 'New record created successfully'
-    #return jsonify(msg)
+# return render_template("Patients.html")
 
-@app.route("/ajax_delete",methods=["POST","GET"])
+# the method which is used to delete
+
+# @app.route("/ajax_add",methods=["POST","GET"])
+# def ajax_add():
+# if request.method == 'POST':
+# cur = db.connection.cursor(MySQLdb.cursors.DictCursor)
+# patients_first_name = request.form['patients_first_name']
+# patients_last_name = request.form['patients_last_name']
+###Patient_address = request.form['Patient_address']
+# Patients_email_address = request.form['Patients_email_address']
+# patients_phone_number = request.form['patients_phone_number']
+# print(txtname)
+# if patients_first_name == '':
+# msg = 'Please Input name'
+# elif patients_last_name == '':
+# msg = 'Please Input Department'
+# elif Patients_father_name == '':
+# msg = 'Please Input Phone'
+# elif Patient_address == '':
+# msg = 'Please Input Department'
+# elif Patients_email_address == '':
+# msg = 'Please Input Phone'
+# elif patients_phone_number == '':
+# msg = 'Please Input Phone'
+# else:
+# cur.execute(
+# "INSERT INTO logininfo.patients(patients_first_name,patients_last_name,Patients_father_name,Patient_address,Patients_email_address,patients_phone_number) VALUES(%s,%s,%s,%s,%s,%s)",
+# (patients_first_name, patients_last_name, Patients_father_name, Patient_address,Patients_email_address,patients_phone_number))
+# db.connection.commit()
+# cur.close()
+# msg = 'New record created successfully'
+# return jsonify(msg)
+
+@app.route("/ajax_delete", methods=["POST", "GET"])
 def ajax_delete():
     cur = db.connection.cursor(MySQLdb.cursors.DictCursor)
     if request.method == 'POST':
@@ -204,63 +205,68 @@ def ajax_delete():
         msg = 'Record deleted successfully'
     return jsonify(msg)
 
-@app.route("/ajax_update",methods=["POST","GET"])
+
+@app.route("/ajax_update", methods=["POST", "GET"])
 def ajax_update():
-    cur = db.connection.cursor(MySQLdb.cursors.DictCursor)
     if request.method == 'POST':
+        cur = db.connection.cursor(MySQLdb.cursors.DictCursor)
         string = request.form['string']
-        patients_first_name = request.form['patients_first_name']
-        patients_last_name = request.form['patients_last_name']
+        patients_f_name = request.form['patients_f_name']
+        patients_l_name = request.form['patients_l_name']
         Patients_father_name = request.form['Patients_father_name']
         Patient_address = request.form['Patient_address']
         Patients_email_address = request.form['Patients_email_address']
         patients_phone_number = request.form['patients_phone_number']
         print(string)
-        cur.execute("UPDATE patients SET patients_first_name = %s, patients_last_name = %s, Patients_father_name = %s,Patient_address = %s, Patients_email_addres = %s, patients_phone_number = %s WHERE idPatients = %s ", [patients_first_name,patients_last_name,Patients_father_name,Patient_address,Patients_email_address,patients_phone_number, string])
+        if patients_f_name == '' and patients_l_name == '' and Patients_father_name == '' and Patient_address == '' and Patients_email_address == '' and patients_phone_number == '':
+            msg = 'Please Input first_name'
+            return jsonify(msg)
+        elif patients_f_name == '':
+            msg = 'Please Input first_name'
+            return jsonify(msg)
+        elif patients_l_name == '':
+            msg = 'Please Input Last_name'
+            return jsonify(msg)
+        elif Patients_father_name == '':
+            msg = 'Please Input Father_name'
+            return jsonify(msg)
+        elif Patient_address == '':
+            msg = 'Please Input Address'
+            return jsonify(msg)
+        elif Patients_email_address == '':
+            msg = 'Please Input Email_Address'
+            return jsonify(msg)
+        elif patients_phone_number == '':
+            msg = 'Please Input Phone_Number'
+            return jsonify(msg)
+        else:
+            cur.execute(
+                "UPDATE logininfo.patients SET patients_first_name = %s, patients_last_name = %s, Patients_father_name = %s,Patient_address = %s, Patients_email_address = %s, patients_phone_number = %s WHERE idPatients = %s ",
+                [patients_f_name, patients_l_name, Patients_father_name, Patient_address, Patients_email_address,
+                 patients_phone_number, string])
         db.connection.commit()
         cur.close()
+        print(cur)
         msg = 'Record successfully Updated'
     return jsonify(msg)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Delete User
-#@app.route("/deleteUser/<string:id>",methods=['GET','POST'])
-#def deleteUser(id):
-    #cur = db.connection.cursor(MySQLdb.cursors.DictCursor)
-    #cur.execute("delete  from patients where idPatients=%s",([id],))
-    #db.connection.commit()
-    #cur.close()
-    #cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
-    #patients_RECORD = cursor.execute("SELECT * FROM patients ")
-    # patients_RECORD = cursor.execute("SELECT * FROM logininfo.patients WHERE patients_first_name=%s", ([patients]))
-    #"""Api to retive all the patient from the database"""
-    #if (patients_RECORD) > 0:
-        #user_details = cursor.fetchall()
-        #print(user_details)
-        # db.connection.commit()
-    ##return render_template("Patients.html", user_details=user_details)
-
-
-
-
+# Delete User
+# @app.route("/deleteUser/<string:id>",methods=['GET','POST'])
+# def deleteUser(id):
+# cur = db.connection.cursor(MySQLdb.cursors.DictCursor)
+# cur.execute("delete  from patients where idPatients=%s",([id],))
+# db.connection.commit()
+# cur.close()
+# cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+# patients_RECORD = cursor.execute("SELECT * FROM patients ")
+# patients_RECORD = cursor.execute("SELECT * FROM logininfo.patients WHERE patients_first_name=%s", ([patients]))
+# """Api to retive all the patient from the database"""
+# if (patients_RECORD) > 0:
+# user_details = cursor.fetchall()
+# print(user_details)
+# db.connection.commit()
+##return render_template("Patients.html", user_details=user_details)
 
 
 if __name__ == '__main__':
